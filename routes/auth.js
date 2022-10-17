d8bcd9cf859a294dbfbbc218dfca5a38defacd93
overlord <rexatuzie@gmail.com>
2022-10-17 08:49:09 +0400

    monday commit 1

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,8 @@ import express from 'express';
 import { getUsers, login, register } from '../controllers/auth.js';
 const router = express.Router();
 
+console.log('local');
+
 router.get('/', getUsers);
 router.post('/register', register);
 router.get('/verify-email', register);

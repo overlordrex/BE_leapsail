c8df71a717eb97f136e2f3f4010e3e92fad3bdb3
overlord <rexatuzie@gmail.com>
2022-10-31 09:54:22 +0400

    monday commit 1

--- models/User.js
+++ models/User.js
@@ -5,7 +5,7 @@ const UserSchema = new mongoose.Schema(
     firstname: { type: String },
     lastname: { type: String },
     email: { type: String, unique: true },
-    phoneNumber: { type: Number },
+    phoneNumber: { type: String },
     password: { type: String },
     emailToken: { type: String },
     verified: { type: Boolean, default: false },
